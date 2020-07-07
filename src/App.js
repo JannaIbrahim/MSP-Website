373aad8a5b915ba8736778ad827ded2b0f146774
EsraaElshibly <61299088+EsraaElshibly@users.noreply.github.com>
2020-07-07 20:06:54 +0200

    Update App.js
    This is code for counters

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import Counter from "./components/home/Counter/Counter"
 
 function App() {
-  return <div className="App"></div>;
+  return (
+  <div className="App">
+    <Counter />
+  </div>
+  )
 }
 
 export default App;
