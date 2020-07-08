aba45a3177ac223f24b0a1c7707f0f25edfa3635
Ahmed Gamal <ahmedgamal.21724@gmail.com>
2020-07-08 20:46:57 +0200

    edit slider

--- src/components/home/BestMembers/BestMembers.js
+++ src/components/home/BestMembers/BestMembers.js
@@ -1,44 +1,83 @@
 import React,{Component} from 'react';
 import "./BestMembers.css"
 class BestMembers extends Component {
-    // state = {  }
+     state = 
+     {
+     img:['/images/a.jpg','/images/b.jpg','/images/c.jpg',
+          '/images/d.jpg','/images/e.jpg','/images/f.jpg',
+          '/images/g.jpg','/images/m.jpg','/images/l.jpg'],
+     count0:0,  
+     count1:1,  
+     count2:2,  
+     }
+
+  
+      
+     componentWillMount()
+     {
+         setInterval(() => {
+        
+            if(this.state.count0<this.state.img.length)
+            {
+                this.setState({ count0:this.state.count0+3});
+            }  else     this.setState({ count0: this.state.count0=0 });
+            
+
+
+
+            if(this.state.count1<this.state.img.length)
+            {
+                this.setState({ count1:this.state.count1+3});
+            }  else    this.setState({ count1: this.state.count1=1 });
+
+
+
+            if(this.state.count2<this.state.img.length)
+            {
+                this.setState({ count2:this.state.count2+3});
+            }  else this.setState({ count2: this.state.count2=2 });
+
+            }, 2000)
+
+            
+     }
     
     render() { 
+     
         return ( 
+            
   <div className="container" >
-            {/* {console.log(`w${window.outerWidth}`)}  
-            {console.log(`H${window.outerHeight}`)}   */}
-                        
-    
-                                                     
+                                                 
        <div className="row">
 
+
          <div className="col">
-             <img src="/images/vectortwins160200158.jpg"  className="img_member" />
+                    <img src={this.state.img[this.state.count0]}  className="img_member" />
              <h6 className="name_member">Ahmed Gamal</h6>
              <h6 className="sub_member">Web</h6>
              
          </div>
 
         <div className="col ">
-            <img src="/images/vectortwins160200158.jpg" className="img_member"/>
+            <img src={this.state.img[this.state.count1]} className="img_member"/>
             <h6 className="name_member">Ahmed Gamal</h6>
             <h6 className="sub_member">Web</h6>
             
         </div>
 
        <div className="col">
-           <img src="/images/vectortwins160200158.jpg" className="img_member"/>
+           <img src={this.state.img[this.state.count2]} className="img_member"/>
            <h6 className="name_member">Ahmed Gamal</h6>
            <h6 className="sub_member">Web</h6>
      
        </div>
 
       </div>
-
-    
+      
 
 </div>
+
+
 );
     }
 }
