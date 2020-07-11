e8f77dce3ecee9969fbdb956074ce6f4960317d4
EsraaElshibly <61299088+EsraaElshibly@users.noreply.github.com>
2020-07-11 02:15:15 +0200

    The parent Counter's component
    adding all div in the website

--- src/components/home/Counter/Counter.js
+++ src/components/home/Counter/Counter.js
@@ -1,43 +1,43 @@
 import React, { Component } from "react";
 import "./Counter.css";
-import FirstCounter from "./FirstCounter";
-import SecondCounter from "./SecondCounter";
-import ThirdCounter from "./ThirdCounter";
-import FourthCounter from "./FourthCounter";
+import AllCounters from "./AllCounters";
+
 
 class Counter extends Component {
   render() {
     return (
       <div className="counter">
+      {/******************* Start Container Div **************}
         <div className="container">
           {/*************** Start row div for responsive ***************/}
           <div className="row content-of-icons ">
-            {/******* Start First div for Icons and paragraph ********/}
-            <div className="child-one child col-md-3 col-sm-6 col-12 ">
-              <FirstCounter />
+            {/******* Start Person Icons and paragraph ********/}
+            <div className="parent col-md-3 col-sm-6 col-12 ">
+              <AllCounters icon="persons fas fa-user-friends" num="55000" text="Follower" />
             </div>
-            {/******* End First div for Icons and paragraph ********/}
+            {/******* End Person Icons and paragraph ********/}
 
-            {/******* Start Second div for Icons and paragraph ********/}
-            <div className="child-one child col-md-3 col-sm-6 col-12 ">
-              <SecondCounter />
+            {/******* Start Graduation Icons and paragraph ********/}
+            <div className="parent col-md-3 col-sm-6 col-12 ">
+              <AllCounters icon="fa fa-graduation-cap" num="4000" text="Student" />
             </div>
-            {/******* End Second div for Icons and paragraph ********/}
+            {/******* End Graduation Icons and paragraph ********/}
 
-            {/******* Start Third div for Icons and paragraph ********/}
-            <div className="child-one child col-md-3 col-sm-6 col-12 ">
-              <ThirdCounter />
+            {/******* Start Camp Icons and paragraph ********/}
+            <div className="parent col-md-3 col-sm-6 col-12 ">
+              <AllCounters icon="fas fa-archway" num="200" text="Camp" />
             </div>
-            {/******* End Third div for Icons and paragraph ********/}
+            {/******* End Camp Icons and paragraph ********/}
 
-            {/******* Start Fourth div for Icons and paragraph ********/}
-            <div className="child-one child col-md-3 col-sm-6 col-12 ">
-              <FourthCounter />
+            {/******* Start Location Icons and paragraph ********/}
+            <div className="parent col-md-3 col-sm-6 col-12 ">
+              <AllCounters icon="fas fa-map-marker-alt" num="70" text="Event" />
             </div>
-            {/******* End Fourth div for Icons and paragraph ********/}
+            {/******* End Location Icons and paragraph ********/}
           </div>
           {/*************** End row div for responsive ***************/}
         </div>
+      {/******************* End Container Div **************}
       </div>
     );
   }
