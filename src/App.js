830f1694b64ede7845a7a271db993f40f7d3d58c
EsraaShbly <68352564+EsraaShbly@users.noreply.github.com>
2020-07-22 01:39:14 +0200

    Showing DashboardHomePage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import DashboardHomePage from "./components/dashboard/dashboardHome/DashboardHomePage/DashboardHomePage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <DashboardHomePage />
+    </div>
+  );
 }
 
 export default App;
+
