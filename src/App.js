5f5743a3f224460e28d3b6a40a672b39922dd8e1
EsraaShbly <68352564+EsraaShbly@users.noreply.github.com>
2020-08-07 22:44:18 +0200

    Adding Page For About

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import DashboardHomePage from "./components/dashboard/dashboardHome/DashboardHomePage/DashboardHomePage";
+import AboutUs from "./components/home/AboutSection/AboutSection"
 
 function App() {
   return (
     <div className="App">
       <DashboardHomePage />
+      <AboutUs />
     </div>
   );
 }
