02ba41651d0b2c069ea2a31dcf1d0c399650c865
alaahussein22 <56509626+alaahussein22@users.noreply.github.com>
2020-09-12 00:53:49 +0200

    Update SingleEventPage.js
    add singleeventpage.js

---
+++ src/components/events/SingleEventPage/SingleEventPage.js
@@ -0,0 +1,109 @@
+import React, {Component} from "react";
+import "./SingleEventPage.css";
+
+
+function SingleEventPage() {
+  return (
+  
+  <div className="event">
+     <div className="container">
+     <div className="row">
+
+       <div className="col-lg-12 col-md-12 col-sm-6">
+
+       <div className="icons">
+        <span> <i class="far fa-calendar-alt"></i> 15-16/2/2020 at 10 am </span><br></br>
+
+        <span> <i class="far fa-money-bill-alt"></i> free</span><br></br>
+      </div>
+
+     <p>
+
+     "𝕂𝕟𝕠𝕨𝕝𝕖𝕕𝕘𝕖 𝕚𝕤 𝕟𝕠𝕥 𝕖𝕟𝕠𝕦𝕘𝕙 𝕨𝕖 𝕞𝕦𝕤𝕥 𝕒𝕡𝕡𝕝𝕪".<br></br>
+And so we liked to leave the college and study department, and from the pure knowledge<br></br>
+that we take from books, it alone will not be enough to support us after graduation,<br></br>
+and from here was the beginning of our idea.<br></br>
+How do we balance between the knowledge we have that we need after graduation ?! ...<br></br>
+In order to deviate from the usual routine, the ExKnow Forum is not just a speaker<br></br>
+explaining, and we hear or receive information, but, the EVENT is ...<br></br>
+𝐏𝐚𝐧𝐞𝐥 𝐃𝐢𝐬𝐜𝐮𝐬𝐬𝐢𝐨𝐧 <br></br>
+A discussion takes place between the attendees and the experts who are present with us,<br></br>
+in order to make sure that they have really reached all your needs and answered all the questions.<br></br>
+We gathered for you the most pioneering lecturers in their magazines, in order to pass <br></br>
+on the experiences and experiences that they passed through during their lives to us.<br></br>
+And these areas through which we linked technology to the skills and transactions that<br></br> 
+we need in order to start in the field with our own or even our own project, and the fields will be:<br></br>
+◾ IOT <br></br>
+◾ Cloud Computing <br></br>
+◾ Software <br></br>
+◾ Banking   <br></br>
+◾ Business  <br></br>
+◾ Management  <br></br>
+Since experience does not come only through experience, so it will remain among the most <br></br>
+important paragraphs of our event. This is the (StartUps) paragraph.<br></br>
+And the countries of the people who started their own projects, they started from scratch <br></br>
+until they reached their idea of ​​light. They will talk to us about the challenges that I<br></br>
+ met and how they were able to overcome them.<br></br>
+As you are used to from the huge opportunities that we provide, it had to be in a great<br></br>
+ place in order to be worthy of the ExKnow Forum.<br></br>
+And so the most appropriate choice was the hall of the British University in Egypt.<br></br>
+Event Tickets: 𝖋𝖔𝖗 𝖋𝖗𝖊𝖊 “Absolutely Free” 😌🔥<br></br>
+Registration Form Link: https://bit.ly/30DjyPM <br></br>
+#ExKnow_Forum <br></br>
+#MakeyourDecision_ChooseyourCareer <br></br>
+# Make yourself motivation to start Show less <br></br>
+
+     </p>
+
+
+
+
+       <p>
+
+       "𝕂𝕟𝕠𝕨𝕝𝕖𝕕𝕘𝕖 𝕚𝕤 𝕟𝕠𝕥 𝕖𝕟𝕠𝕦𝕘𝕙 𝕨𝕖 𝕞𝕦𝕤𝕥 𝕒𝕡𝕡𝕝𝕪".<br></br>
+                       وعشان كده حبينا نخرج من دائرة الكلية والدراسة، ومن العلم البحت الي بناخده         
+      <br></br> من الكتب، لأنها لوحدها مش كفاية عشان تسندنا بعد التخرج، ومن هنا كانت بداية فكرتنا
+      <br></br>         إزاي هنوازن ما بين المعرفة الي عندنا والخبرة الي هنحتاجها بعد التخرج؟!...
+      <br></br>                                  وعشان نبعد عن الروتين المعتاد فأي إيفنت،حبينا نخلي 
+      <br></br>(ExKnow Forum) مش مجرد speaker بيشرح واحنا بنسمع او بنستقبل معلومات بس، الايفنت عباره عن...
+      <br></br>     𝐏𝐚𝐧𝐞𝐥 𝐃𝐢𝐬𝐜𝐮𝐬𝐬𝐢𝐨𝐧
+      <br></br> نقاش بيتم بين الحضور وأصحاب الخبرة الي موجودين معانا، ودا عشان نتأكد انهم فعلا وصلوا كل  
+      <br></br>  احتياجاتكم واجابواعلى كل الاسئله
+      <br></br>      فجمعنا لكم اكتر مُحاضرين رائدين فمجالتهم، 
+      <br></br>           عشان ينقلوا الخبرات والتجارب الي مروا بيها خلال حياتهم لنا.
+      <br></br>       والمجالات دي ربطنا من خلالها التكنولوجيا بالمهارات والمعاملات الي بنحتاجها عشان نبدأ
+      <br></br>                                   : في المجال بتاعنا او حتى مشروعنا الخاص، والمجالات هتكون
+◾ IOT <br></br>
+◾ Cloud Computing <br></br>
+◾ Software <br></br>
+◾ Banking <br></br>
+◾ Business <br></br>
+◾ Management <br></br>
+<br></br>                    وبما ان الخبرة مش بتيجي غير بالتجربة، عشان كده هيبقى
+<br></br>                 من ضمن اهم فقرات الإيفنت بتاعنا هيا فقرة ال( StartUps).
+<br></br>ودول الناس الي بدأوا مشاريعهم الخاصة، بدأوها من الصفر لحد ما وصلوا بفكرتهم للنور،
+<br></br>            .هيكلمونا عن التحديات الي قابلتهم وازاي قدروا يتغلبو عليها
+<br></br>                         , وزي ما اتعودتم مننا الفرص الضخمة الي بنوفرها
+<br></br>               كان لازم يكون في مكان عظيم عشان يليق بمقام( ExKnow Forum ).
+<br></br>               وعشان كده الاختيار الأنسب كان قاعة الجامعة البريطانية بمصر،
+<br></br>                  هنستناكم هناك يومي 15 & 16 من شهر فبراير.
+Event Tickets: 𝖋𝖔𝖗 𝖋𝖗𝖊𝖊 "مجاناً تماماً" 😌🔥 <br></br>
+Registration Form Link: https://bit.ly/30DjyPM<br></br>
+#ExKnow_Forum<br></br>
+#MakeyourDecision_ChooseyourCareer<br></br>
+<br></br> #فاصنع_لنفسك_دافعاً_لتبدأ ❤️✨ عرض أقل
+       </p>
+       
+       <button>ENROLL NOW</button>
+       
+</div>
+        
+</div>
+
+     </div>
+     
+    </div>
+
+  )};
+
+  export default SingleEventPage;
